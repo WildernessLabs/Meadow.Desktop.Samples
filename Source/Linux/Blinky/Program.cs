12235c23b032793c0fa2817f251897aaf521ae60
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-07-02 23:40:55 -0700

    Add Linux Blinky and template project

---
+++ Source/Linux/Blinky/Program.cs
@@ -0,0 +1,27 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Pinouts;
+
+public class MeadowApp : App<Linux<RaspberryPi>>
+{
+    static async Task Main(string[] args)
+    {
+        await MeadowOS.Start(args);
+    }
+
+    public override Task Initialize()
+    {
+        Resolver.Log.Info("Initialize...");
+
+        return base.Initialize();
+    }
+
+    public override Task Run()
+    {
+        Resolver.Log.Info("Run...");
+
+        Resolver.Log.Info("Hello, Meadow.Linux!");
+
+        return base.Run();
+    }
+}
