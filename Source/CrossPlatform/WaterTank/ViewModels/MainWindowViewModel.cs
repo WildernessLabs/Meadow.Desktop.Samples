3bef1473b829cb85750286051c1610d9b612bb37
Chris Tacke <ctacke@opennetcf.com>
2023-07-12 20:17:35 -0500

    added basic binding to tank UI

--- Source/CrossPlatform/WaterTank/ViewModels/MainWindowViewModel.cs
+++ Source/CrossPlatform/WaterTank/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,31 @@
-﻿namespace WaterTank.ViewModels;
+﻿using ReactiveUI;
+
+namespace WaterTank.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private int _fillPercent;
+
+    public int FillPercentage
+    {
+        get => _fillPercent;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _fillPercent, value);
+            this.RaisePropertyChanged(nameof(FillHeight));
+            this.RaisePropertyChanged(nameof(WaterTop));
+        }
+    }
+
+    public int TankHeight { get; set; } = 600;
+
+    public int FillHeight
+    {
+        get => TankHeight * FillPercentage / 100;
+    }
+
+    public int WaterTop
+    {
+        get => TankHeight - FillHeight;
+    }
 }
