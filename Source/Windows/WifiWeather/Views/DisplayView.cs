6331acba4e029d87dcc73042beb3483dd0cd0492
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-10-18 22:33:01 -0700

    Update weather HMI to MicroLayout

--- Source/Windows/WifiWeather/Views/DisplayView.cs
+++ Source/Windows/WifiWeather/Views/DisplayView.cs
@@ -1,132 +1,250 @@
-﻿using Meadow.Foundation;
-using Meadow.Foundation.Graphics;
-using SimpleJpegDecoder;
-using System.Reflection;
+﻿using Meadow.Foundation.Graphics;
+using Meadow.Foundation.Graphics.MicroLayout;
 
 namespace WifiWeather.Views
 {
     public class DisplayView
     {
-        MicroGraphics graphics;
-        int x_padding = 5;
+        Image _weatherIcon = Image.LoadFromResource("WifiWeather.w_clear.bmp");
+
+        int x_padding = 20;
+
+        protected DisplayScreen DisplayScreen { get; set; }
+
+        protected Label DayOfWeek { get; set; }
+
+        protected Label Month { get; set; }
+
+        protected Label Year { get; set; }
+
+        protected Label Time { get; set; }
+
+        protected Picture Weather { get; set; }
+
+        protected Label Temperature { get; set; }
+
+        protected Label Humidity { get; set; }
+
+        protected Label Pressure { get; set; }
+
+        protected Label FeelsLike { get; set; }
+
+        protected Label WindDirection { get; set; }
+
+        protected Label WindSpeed { get; set; }
 
         Meadow.Foundation.Color backgroundColor = Meadow.Foundation.Color.FromHex("#F3F7FA");
         Meadow.Foundation.Color foregroundColor = Meadow.Foundation.Color.Black;
 
         Font12x20 font12X20 = new Font12x20();
+        Font12x16 font12X16 = new Font12x16();
         Font8x16 font8X16 = new Font8x16();
 
         public DisplayView(IGraphicsDisplay display)
         {
-            graphics = new MicroGraphics(display)
+            DisplayScreen = new DisplayScreen(display)
             {
-                Stroke = 1,
-                CurrentFont = font12X20
+                BackgroundColor = backgroundColor
             };
 
-            x_padding = 20;
+            Weather = new Picture(x_padding, 25, 100, 100, _weatherIcon);
+            DisplayScreen.Controls.Add(Weather);
 
-            graphics.Clear(backgroundColor);
+            DayOfWeek = new Label(DisplayScreen.Width / 2 - x_padding, 25, DisplayScreen.Width / 2, font12X20.Height)
+            {
+                Text = $"Monday,1st",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font12X20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Right
+            };
+            DisplayScreen.Controls.Add(DayOfWeek);
+
+            Month = new Label(DisplayScreen.Width / 2 - x_padding, 50, DisplayScreen.Width / 2, font12X20.Height * 2, ScaleFactor.X2)
+            {
+                Text = $"Jan",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font12X20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Right
+            };
+            DisplayScreen.Controls.Add(Month);
+
+            Year = new Label(DisplayScreen.Width / 2 - x_padding, 95, DisplayScreen.Width / 2, font12X20.Height * 2, ScaleFactor.X2)
+            {
+                Text = $"0000",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font12X16,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Right
+            };
+            DisplayScreen.Controls.Add(Year);
+
+            Time = new Label(0, 150, DisplayScreen.Width, font12X20.Height * 2, ScaleFactor.X2)
+            {
+                Text = $"00:00:00 AM",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font12X20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            DisplayScreen.Controls.Add(Time);
+
+            DisplayScreen.Controls.Add(new Label(x_padding, 215, DisplayScreen.Width / 2, font12X20.Height * 2)
+            {
+                Text = $"Temperature",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font12X20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Left
+            });
+            DisplayScreen.Controls.Add(new Label(x_padding, 304, DisplayScreen.Width / 2, font12X20.Height * 2)
+            {
+                Text = $"Humidity",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font12X20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Left
+            });
+            DisplayScreen.Controls.Add(new Label(x_padding, 393, DisplayScreen.Width / 2, font12X20.Height * 2)
+            {
+                Text = $"Pressure",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font12X20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Left
+            });
+            DisplayScreen.Controls.Add(new Label(DisplayScreen.Width / 2 + x_padding, 215, DisplayScreen.Width / 2 - x_padding * 2, font12X20.Height * 2)
+            {
+                Text = $"Feels like",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font12X20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Right
+            });
+            DisplayScreen.Controls.Add(new Label(DisplayScreen.Width / 2 - x_padding, 304, DisplayScreen.Width / 2, font12X20.Height * 2)
+            {
+                Text = $"Wind Dir",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font12X20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Right
+            });
+            DisplayScreen.Controls.Add(new Label(DisplayScreen.Width / 2 - x_padding, 393, DisplayScreen.Width / 2, font12X20.Height * 2)
+            {
+                Text = $"Wind Spd",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font12X20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Right
+            });
+
+            Temperature = new Label(x_padding, 245, DisplayScreen.Width / 2, font12X20.Height * 2, ScaleFactor.X2)
+            {
+                Text = $"0°C",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font12X20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Left
+            };
+            DisplayScreen.Controls.Add(Temperature);
+            Humidity = new Label(x_padding, 334, DisplayScreen.Width / 2, font12X20.Height * 2, ScaleFactor.X2)
+            {
+                Text = $"0%",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font12X20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Left
+            };
+            DisplayScreen.Controls.Add(Humidity);
+            Pressure = new Label(x_padding, 423, DisplayScreen.Width / 2, font12X20.Height * 2, ScaleFactor.X2)
+            {
+                Text = $"0hPa",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font8X16,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Left
+            };
+            DisplayScreen.Controls.Add(Pressure);
+
+            FeelsLike = new Label(DisplayScreen.Width / 2 - x_padding, 245, DisplayScreen.Width / 2, font12X20.Height * 2, ScaleFactor.X2)
+            {
+                Text = $"0°C",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font12X20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Right
+            };
+            DisplayScreen.Controls.Add(FeelsLike);
+            WindDirection = new Label(DisplayScreen.Width / 2 - x_padding, 334, DisplayScreen.Width / 2, font12X20.Height * 2, ScaleFactor.X2)
+            {
+                Text = $"0°",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font12X20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Right
+            };
+            DisplayScreen.Controls.Add(WindDirection);
+            WindSpeed = new Label(DisplayScreen.Width / 2 - x_padding, 423, DisplayScreen.Width / 2, font12X20.Height * 2, ScaleFactor.X2)
+            {
+                Text = $"0m/s",
+                TextColor = foregroundColor,
+                BackColor = backgroundColor,
+                Font = font8X16,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Right
+            };
+            DisplayScreen.Controls.Add(WindSpeed);
         }
 
         private static string GetOrdinalSuffix(int num)
         {
             string number = num.ToString();
-            if (number.EndsWith("11")) return "th";
-            if (number.EndsWith("12")) return "th";
-            if (number.EndsWith("13")) return "th";
             if (number.EndsWith("1")) return "st";
             if (number.EndsWith("2")) return "nd";
             if (number.EndsWith("3")) return "rd";
+            if (number.EndsWith("11")) return "th";
+            if (number.EndsWith("12")) return "th";
+            if (number.EndsWith("13")) return "th";
             return "th";
-        }
-
-        private void DisplayJPG(string weatherIcon, int xOffset, int yOffset)
-        {
-            var jpgData = LoadResource(weatherIcon);
-            var decoder = new JpegDecoder();
-            var jpg = decoder.DecodeJpeg(jpgData);
-
-            int x = 0;
-            int y = 0;
-            byte r, g, b;
-
-            for (int i = 0; i < jpg.Length; i += 3)
-            {
-                r = jpg[i];
-                g = jpg[i + 1];
-                b = jpg[i + 2];
-
-                graphics.DrawPixel(x + xOffset, y + yOffset, Meadow.Foundation.Color.FromRgb(r, g, b));
-
-                x++;
-                if (x % decoder.Width == 0)
-                {
-                    y++;
-                    x = 0;
-                }
-            }
-        }
-
-        private byte[] LoadResource(string weatherIcon)
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-
-            using (Stream stream = assembly.GetManifestResourceStream(weatherIcon))
-            {
-                using (var ms = new MemoryStream())
-                {
-                    stream.CopyTo(ms);
-                    return ms.ToArray();
-                }
-            }
         }
 
         public void UpdateDateTime()
         {
             var today = DateTime.Now;
 
-            graphics.DrawRectangle(graphics.Width / 2, 24, graphics.Width, 82, backgroundColor, true);
-
-            graphics.CurrentFont = font12X20;
-            graphics.DrawText(graphics.Width - x_padding, 25, $"{today.DayOfWeek},{today.Day}{GetOrdinalSuffix(today.Day)}", foregroundColor, alignmentH: HorizontalAlignment.Right);
-            graphics.DrawText(graphics.Width - x_padding, 50, today.ToString("MMM"), foregroundColor, scaleFactor: ScaleFactor.X2, alignmentH: HorizontalAlignment.Right);
-            graphics.DrawText(graphics.Width - x_padding, 95, today.ToString("yyyy"), foregroundColor, alignmentH: HorizontalAlignment.Right);
-
-            graphics.DrawRectangle(0, 135, graphics.Width, 35, backgroundColor, true);
-            graphics.DrawText(graphics.Width / 2, 135, today.ToString("hh:mm:ss tt"), foregroundColor, ScaleFactor.X2, alignmentH: HorizontalAlignment.Center);
-
-            graphics.Show();
+            DayOfWeek.Text = $"{today.DayOfWeek},{today.Day}{GetOrdinalSuffix(today.Day)}";
+            Month.Text = $"{today.ToString("MMMM").Substring(0, today.ToString("MMMM").Length > 6 ? 7 : today.ToString("MMMM").Length)}";
+            Year.Text = $"{today.ToString("yyyy")}";
+            Time.Text = DateTime.Now.ToString("hh:mm:ss tt");
         }
 
         public void UpdateDisplay(string weatherIcon, string temperature, string humidity, string pressure, string feelsLike, string windDirection, string windSpeed)
         {
-            int spacing = 95;
-            int valueSpacing = 30;
-            int y = 200;
+            _weatherIcon = Image.LoadFromResource(weatherIcon);
+            Weather.Image = _weatherIcon;
 
-            graphics.Clear(backgroundColor);
-
-            DisplayJPG(weatherIcon, x_padding, 15);
-
-            graphics.CurrentFont = font12X20;
-            graphics.DrawText(x_padding, y, "Temperature", foregroundColor);
-            graphics.DrawText(x_padding, y + spacing, "Humidity", foregroundColor);
-            graphics.DrawText(x_padding, y + spacing * 2, "Pressure", foregroundColor);
-            graphics.DrawText(graphics.Width - x_padding, y, "Feels like", foregroundColor, alignmentH: HorizontalAlignment.Right);
-            graphics.DrawText(graphics.Width - x_padding, y + spacing, "Wind Dir", foregroundColor, alignmentH: HorizontalAlignment.Right);
-            graphics.DrawText(graphics.Width - x_padding, y + spacing * 2, "Wind Spd", foregroundColor, alignmentH: HorizontalAlignment.Right);
-
-            graphics.DrawText(x_padding, y + valueSpacing, $"{temperature}", foregroundColor, ScaleFactor.X2);
-            graphics.DrawText(graphics.Width - x_padding, y + valueSpacing, $"{feelsLike}", foregroundColor, ScaleFactor.X2, alignmentH: HorizontalAlignment.Right);
-            graphics.DrawText(x_padding, y + valueSpacing + spacing, $"{humidity}", foregroundColor, ScaleFactor.X2);
-            graphics.DrawText(graphics.Width - x_padding, y + valueSpacing + spacing, $"{windDirection}", foregroundColor, ScaleFactor.X2, alignmentH: HorizontalAlignment.Right);
-
-            graphics.CurrentFont = font8X16;
-            graphics.DrawText(graphics.Width - x_padding, y + valueSpacing + spacing * 2, $"{windSpeed}", foregroundColor, ScaleFactor.X2, alignmentH: HorizontalAlignment.Right);
-            graphics.DrawText(x_padding, y + valueSpacing + spacing * 2, $"{pressure}", foregroundColor, ScaleFactor.X2);
-
-            graphics.Show();
+            Temperature.Text = temperature;
+            Humidity.Text = humidity;
+            Pressure.Text = pressure;
+            FeelsLike.Text = feelsLike;
+            WindDirection.Text = windDirection;
+            WindSpeed.Text = windSpeed;
         }
     }
 }
