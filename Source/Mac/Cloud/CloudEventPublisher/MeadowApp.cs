a42097915bff5bfb6dbb3a47fd899357b958ee5d
Chris Tacke <ctacke@opennetcf.com>
2023-12-05 16:24:49 -0600

    Mac samples

---
+++ Source/Mac/Cloud/CloudEventPublisher/MeadowApp.cs
@@ -0,0 +1,58 @@
+﻿using Meadow;
+using Meadow.Cloud;
+using Meadow.Logging;
+
+public class MeadowApp : App<Mac>
+{
+    public static async Task Main(string[] args)
+    {
+        await MeadowOS.Start(args);
+    }
+
+    private DateTimeOffset _startTime;
+
+    public override Task Initialize()
+    {
+        _startTime = DateTimeOffset.UtcNow;
+
+        Resolver.Log.AddProvider(new DebugLogProvider());
+        Resolver.MeadowCloudService.ServiceError += OnCloudServiceError;
+
+        return base.Initialize();
+    }
+
+    private void OnCloudServiceError(object? sender, string e)
+    {
+        Console.WriteLine($"CLOUD ERROR: {e}");
+    }
+
+    public override async Task Run()
+    {
+        var eventNumber = 20;
+
+        while (true)
+        {
+            var @event = new CloudEvent
+            {
+                Measurements = new()
+                 {
+                     { "uptime", DateTime.UtcNow - _startTime },
+                     { "eventNumber", eventNumber++ },
+                     { "text", "foo bar" }
+                 }
+            };
+
+            try
+            {
+                Console.WriteLine($"Sending event to Meadow.Cloud...");
+                await Resolver.MeadowCloudService.SendEvent(@event);
+            }
+            catch (Exception ex)
+            {
+                Resolver.Log.Error($"ERROR: {ex.Message}");
+            }
+
+            await Task.Delay(10000);
+        }
+    }
+}
